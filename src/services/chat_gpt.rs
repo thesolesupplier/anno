2ceff132d6e7b2e0567f87d8a67a7de20ee24a57
Constantin Cerdan <constantincerdan@gmail.com>
2024-06-05 21:59:29 +0100

    Update chatgpt prompt

--- src/services/chat_gpt.rs
+++ src/services/chat_gpt.rs
@@ -6,20 +6,19 @@ pub async fn get_diff_summary(diff: &str) -> Result<String> {
 
     let chat_gpt = ChatGPT::new(openai_api_key)?;
 
-    let message = format!(
-        "
-         You are a software developer turned product owner.
-         Your responsbility is to communicate the changes in this diff to the team.
-         The team is non-tech and you need to make it easy for them to understand.
-         Rather than describing the diff literally, you need to summarise it.
-         Describe the change as a feature in its entirety rather than the individual
-         lines or files that were changed.
-         Your summary should be concise and no longer than a single sentence.
-         Again, your entire summary should only be a single sentence long, no headings or bullet points
-         ----
-         Diff: {diff}
-        "
-    );
+    let message = format!("
+        You are a software developer turned product owner.
+        Your responsibility is to communicate the changes in this diff to the team.
+        The team is non-tech, and you need to make it easy for them to understand.
+        Rather than describing the diff literally, you need to summarize it.
+        Describe the change as a feature in its entirety rather than the individual
+        lines or files that were changed.
+        Your summary should be concise and no longer than a single sentence.
+        Again, your entire summary should only be a single sentence long, no headings or bullet points.
+        If there are any feature flags, mention which environments they are enabled in and disabled in.
+        ----
+        Diff: {diff}
+    ");
 
     let response = chat_gpt
         .send_message(message)
