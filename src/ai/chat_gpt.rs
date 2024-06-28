e0acd5d74ae6d919517ab95e3c1312d35afa8cdf
Constantin Cerdan <constantincerdan@gmail.com>
2024-06-28 17:55:15 +0100

    Replace dependency on `chatgpt_rs` crate with API request

--- src/ai/chat_gpt.rs
+++ src/ai/chat_gpt.rs
@@ -1,32 +1,63 @@
 use super::prompt::RELEASE_SUMMARY_PROMPT;
-use chatgpt::prelude::*;
+use anyhow::Result;
+use serde::Deserialize;
+use serde_json::json;
 use std::env;
 
 pub async fn summarise_release(diff: &str, commit_messages: &[String]) -> Result<String> {
+    let openai_base_url = env::var("OPENAI_BASE_URL").expect("OPENAI_BASE_URL should be set");
     let openai_api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY should be set");
-
-    let chat_gpt = ChatGPT::new_with_config(
-        openai_api_key,
-        ModelConfigurationBuilder::default()
-            .engine(ChatGPTEngine::Custom("gpt-4o"))
-            .temperature(0.0)
-            .frequency_penalty(2.0)
-            .build()
-            .unwrap(),
-    )?;
 
     let commit_messages = commit_messages.join("\n");
 
-    let response = chat_gpt
-        .send_message(format!(
-            "<Prompt>{RELEASE_SUMMARY_PROMPT}</Prompt>
-             <Diff>{diff}</Diff>
-             <Commit Messages>{commit_messages}<Commit Messages>"
-        ))
+    let mut response = reqwest::Client::new()
+        .post(format!("{openai_base_url}/chat/completions"))
+        .header("content-type", "application/json")
+        .bearer_auth(openai_api_key)
+        .json(&json!({
+            "model": "gpt-4o",
+            "temperature": 0.0,
+            "frequency_penalty": 2.0,
+            "messages": [
+                {
+                    "role": "system",
+                    "content": RELEASE_SUMMARY_PROMPT
+                },
+                {
+                    "role": "user",
+                    "content": format!(
+                        "<Diff>{diff}</Diff>
+                        <Commit Messages>{commit_messages}<Commit Messages>"
+                    )
+                }
+            ]
+        }))
+        .send()
         .await?
-        .message()
-        .content
-        .clone();
+        .error_for_status()?
+        .json::<Response>()
+        .await?;
 
-    Ok(response)
+    Ok(response.get_summary())
+}
+
+#[derive(Deserialize)]
+pub struct Response {
+    choices: Vec<Message>,
+}
+
+impl Response {
+    pub fn get_summary(&mut self) -> String {
+        self.choices.remove(0).message.content
+    }
+}
+
+#[derive(Deserialize)]
+struct Message {
+    message: Content,
+}
+
+#[derive(Deserialize)]
+struct Content {
+    content: String,
 }
