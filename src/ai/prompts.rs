9dcff25fd4d94b9dd94f6e0b508aeaae447b23ae
Constantin Cerdan <constantincerdan@gmail.com>
2024-10-20 18:13:19 +0100

    [chore] Update release summary prompt to include feature flag section

--- src/ai/prompts.rs
+++ src/ai/prompts.rs
@@ -3,7 +3,7 @@ pub const RELEASE_SUMMARY: &str = "
         Your role is to analyze a git code diff and related commit messages to identify and summarize the features that have been released.
         Avoid describing each individual code change. Instead, focus on understanding the broader context of the changes and what features they translate into.
         Keep your description of each feature concise and non-technical, so that a non-technical team member can understand the change in simple terms.
-        Avoid listing every commit message or code change. Instead, group the changes into categories like New features, Improvements, Bug fixes, and Dependency changes.
+        Avoid listing every commit message or code change. Instead, group the changes into categories like New features, Improvements, Bug fixes Dependency changes, Feature flags.
         Avoid describing how a feature will impact a user or experience, just describe what the feature is and what it does.
         Avoid expanding acronyms, for example PLP, PDP or USP, to their full meanings because the users understand those.
         Specify any feature flags connected to these features, if any, along with the environments they are enabled in.
@@ -16,7 +16,7 @@ pub const RELEASE_SUMMARY: &str = "
         Summarize in Non-Technical Terms: Write a summary of these features in a way that a non-technical team can understand, but no longer than a sentence.
         List Dependency Changes: Identify any dependency changes made in the package.json file (e.g., new libraries, updated versions) and list them.
         Identify Feature Flags: Determine which features are controlled by feature flags and specify the environments (qa, staging, prod) in which these flags are enabled.
-        Exclude Unchanged Sections: Only include headings for New features, Improvements, Bug fixes, and Dependency changes if there are updates to list for those headings.
+        Exclude Unchanged Sections: Only include headings for New features, Improvements, Bug fixes, Dependency changes and Feature flags if there are updates to list for those headings.
     </Steps>
     <ExampleOutPut1>
         <Output>
@@ -32,12 +32,16 @@ pub const RELEASE_SUMMARY: &str = "
             *Dependency changes*:
             • Updated Library `XYZ` to version `1.3.0`.
             • Added library `ABC` version `2.1.0`.
+            *Feature flags*
+            • `AVATAR_CUSTOMISATION` is enabled in `qa` and `staging` environments.
         </Output>
     </ExampleOutPut1>
     <ExampleOutPut2>
         <Output>
             *New features*:
             • Added support for tracking URLs in Discord messages for new product discoveries.
+            *Feature flags*
+            • `DISCORD_TRACKED_URLS` is enabled in the `prod` environment.
         </Output>
     </ExampleOutPut2>
     <ExampleOutPut3>
@@ -48,7 +52,7 @@ pub const RELEASE_SUMMARY: &str = "
     </ExampleOutPut3>
     Please perform this analysis on the provided git code diff and commits and deliver a summary as described above based on that diff.
     The output should be placed in <Output> tags as demonstrated in the examples above.
-    Avoid including headings for New features, Improvements, Bug fixes, or Dependency changes if there are no updates to list for those headings.
+    Avoid including headings for New features, Improvements, Bug fixes, Dependency changes or Feature flags if there are no updates to list for those headings.
 ";
 
 pub const PR_ADR_ANALYSIS: &str = "
